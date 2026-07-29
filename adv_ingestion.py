b1d3b4e3a4c3efec7e9bd4a74472bb3989573142
Dev Ghosh <175207023+dev-ghosh@users.noreply.github.com>
2026-07-29 23:34:12 +0530

    Update adv_ingestion.py

--- adv_ingestion.py
+++ adv_ingestion.py
@@ -5,7 +5,7 @@ from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
 from qdrant_db import vectorstore
 
-urls = [
+urls  = [
     "https://lilianweng.github.io/posts/2023-06-23-agent/",
     "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
     "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
